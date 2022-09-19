de033de53dcc317c0010ac60afbb22180353e8b5
Sergio Klemensberger <s.klemensberger@gmail.com>
2022-09-19 10:45:42 +0200

    tabs with content projection

--- src/app/shared/controls/tab/tab.component.ts
+++ src/app/shared/controls/tab/tab.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TabbedPaneComponent } from '../tabbed-pane/tabbed-pane.component';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.scss']
+  styleUrls: ['./tab.component.scss'],
 })
 export class TabComponent implements OnInit {
+  @Input() title = '';
+  visible = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(pane: TabbedPaneComponent) {
+    pane.register(this);
   }
 
+  ngOnInit(): void {}
 }
--- src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabbed-pane',
   templateUrl: './tabbed-pane.component.html',
-  styleUrls: ['./tabbed-pane.component.scss']
+  styleUrls: ['./tabbed-pane.component.scss'],
 })
-export class TabbedPaneComponent implements OnInit {
+export class TabbedPaneComponent implements OnInit, AfterContentInit {
+  tabs: Array<TabComponent> = [];
+  activeTab: TabComponent | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    if (this.tabs.length > 0) {
+      this.activate(this.tabs[0]);
+    }
   }
 
+  ngOnInit(): void {}
+
+  register(tab: TabComponent): void {
+    this.tabs.push(tab);
+  }
+
+  activate(active: TabComponent): void {
+    for (const tab of this.tabs) {
+      tab.visible = tab === active;
+    }
+    this.activeTab = active;
+  }
 }
