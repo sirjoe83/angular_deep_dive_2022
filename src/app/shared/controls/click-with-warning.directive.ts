78b72695fa80e2c0ea19bcd20ca4c73bbf41d798
Sergio Klemensberger <s.klemensberger@gmail.com>
2022-09-26 09:54:20 +0200

    HostBinding

--- src/app/shared/controls/click-with-warning.directive.ts
+++ src/app/shared/controls/click-with-warning.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Directive({
   selector: '[appClickWithWarning]',
 })
 export class ClickWithWarningDirective implements OnInit {
+  @Input() warning = 'Are you sure?';
+  @Output() appClickWithWarning = new EventEmitter();
+
+  @HostBinding('class') classBinding: string | undefined;
+
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
-    this.elementRef.nativeElement.setAttribute('class', 'btn btn-danger');
+    this.classBinding = 'btn btn-danger';
   }
 }
