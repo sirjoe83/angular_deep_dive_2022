3e2f7d41b8b0ae42c83f7bb35180028e506116e2
Sergio Klemensberger <s.klemensberger@gmail.com>
2022-10-05 09:06:58 +0200

    Add basket to aux outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,25 +2,31 @@
 
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { BasketComponent } from './basket/basket.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const APP_ROUTES: Routes = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'basket',
+    component: BasketComponent,
+    outlet: 'aux',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
