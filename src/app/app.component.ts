5369d7a643bdd39638ec426a44477f8e5f641474
Sergio Klemensberger <s.klemensberger@gmail.com>
2022-10-05 09:53:02 +0200

    add loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  GuardsCheckEnd,
+  GuardsCheckStart,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Hello World!';
+  showLoadingIndicator = false;
+
+  constructor(private router: Router) {
+    router.events
+      .pipe(
+        filter(
+          (e) => e instanceof NavigationStart || e instanceof GuardsCheckEnd
+        )
+      )
+
+      .subscribe((event) => {
+        this.showLoadingIndicator = true;
+      });
+
+    router.events
+      .pipe(
+        filter(
+          (e) =>
+            e instanceof NavigationEnd ||
+            e instanceof NavigationError ||
+            e instanceof NavigationCancel ||
+            e instanceof GuardsCheckStart
+        )
+      )
+      .subscribe((event) => {
+        this.showLoadingIndicator = false;
+      });
+  }
 }
